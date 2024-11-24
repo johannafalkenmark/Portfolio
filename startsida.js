0752ad923702f3d286de94f409e5dd9eaa96421a
Johanna Falkenmark <johanna.falkenmark@yh.nackademin.se>
2024-11-24 20:23:46 +0100

    Justerat animation för spraycan

--- startsida.js
+++ startsida.js
@@ -1,11 +1,25 @@
-const myButton=document.getElementById("happening-js");
+const sprayCan = document.getElementById("sprayCan-js");
+const body = document.getElementById("body-js");
+
+// Alt 1
+//const column1 = document.getElementsByClassName("column-1-js");
+//console.log(column1[0]);
+
+// Alt 2
+const column1 = document.querySelector(".column-1-js");
+console.log(column1);
+
+// Alt 3
+
+//const column1=document.getElementById("column-1").className;
 
 let counter = 0;
 
 function clickHandler() {
-counter++;
-console.log(counter);
+  counter++;
+  console.log(counter);
+
+  body.classList.toggle("beige");
 }
 
-
-myButton.addEventListener("click", clickHandler);
+sprayCan.addEventListener("click", clickHandler);
